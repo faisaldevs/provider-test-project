9770f9c67b0cd3bc92661c9af046c3d676c6fb03
Md Faisal Ahamed <faisalah331@gmail.com>
2024-05-20 18:15:43 +0600

    hi

--- lib/riverpod.dart
+++ lib/riverpod.dart
@@ -1,5 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_test_project/stateNotifier.dart';
 
 final riverpodTest = StateProvider((ref) {
   return 0;
+});
+
+final updateValueProvider = StateProvider<String?>((ref){
+
+  return null;
+});
+
+final stateNotifierTest= StateNotifierProvider<StateNotifierClass,int>((ref){
+
+  return StateNotifierClass();
 });
